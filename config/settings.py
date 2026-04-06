833114120d57badb1bef1353d6f665252b35461f
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-04-06 08:22:26 +0530

    Updated the model from base to small to get better speech recognition of whisper model

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 MODEL_NAME = "llama3"
 
-WHISPER_MODEL = "base" # Options: tiny, base, small, medium, large-v2 (https://huggingface.co/openai/whisper-base)
+WHISPER_MODEL = "small" # Options: tiny, base, small, medium, large-v2 (https://huggingface.co/openai/whisper-base)
 
 PIPER_MODEL_PATH = "piper/en_US-lessac-medium.onnx" # https://huggingface.co/spmallick/piper-en_US-lessac-medium
 
